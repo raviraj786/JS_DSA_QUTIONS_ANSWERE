cf38a9b9c7d5b7efaa6bde38b1ee4a119d816b9c
rajravi <rajravi78600@gmail.com>
2025-11-03 20:00:49 +0530

    code nee

---
+++ app.js
@@ -0,0 +1,131 @@
+//  const num = 10;
+// for(let i = 1; i <= num; i++){
+//     console.log(num , i ,  num - i + 1)
+// }
+
+
+
+// 3. print all even number from 1 to N
+
+
+// function evenNum(N){
+    // for(let i  = 1; i <= num ; i++){
+    //     if(i% 2 === 0){
+    //         console.log(i)
+    //     }
+    // }
+
+// }
+
+// evenNum(10)
+
+
+
+// 4. sum first N Natural numbers
+// console.time()
+
+ 
+//  let num = 10000000;
+
+//  optomaize solution
+//  let sum =  num * (num + 1) / 2
+
+
+//  Brute force solution
+// for(let i = 1; i <= num ; i++){
+//     sum = sum + i
+// }
+
+
+//    console.log(sum)
+//   console.timeEnd()
+
+
+
+
+// 5. Product (Factorail)
+// let num = 6
+
+// let fec = 1
+
+// for(let i =  1; i <= num; i++ ){
+//    fec  = fec *  i
+// }
+
+// let res = Math.
+// console.log(fec)
+
+
+
+
+
+// print all number divisible by3 and 5 up to N
+
+// let num = 30 ;
+// for(let i = 1 ; i <= num ; i++){
+//   if(i%3 === 0 && i%5 == 0){
+//     console.log(i)
+//   }
+// }
+
+
+
+
+
+
+// find the sum of all odd numbers up to N
+
+// let sum = 0;
+// for(let i  = 0 ; i <= 10 ; i++){
+//     if(i%2 != 0){
+//         sum = sum + i
+//     }
+// }
+
+//  console.log(sum , 25)
+
+
+
+// print the cubes of Number from 1 to N
+ 
+
+// for(let i = 1 ; i <= 5 ; i++){
+// console.log(i**3)
+// }
+
+// for(let i=1; i<=20; i++){
+//     if(i%2 ===  0  ) {
+//         i 
+//     }
+// }
+
+
+///////////////////////////////////////////////////////////////////
+
+
+// find thr maxium of three number
+
+// const res = Math.max(10 , 20 ,5)
+// console.log(res)
+
+// function maxofthree(a,b,c){
+//     let max;
+//     if(a>b && a>c){
+//        max =a
+//     }else if(b>a&& b>c){
+//        max = b
+//     }else{
+//         max = c
+//     }
+//     return max
+// }
+
+
+
+// console.log(maxofthree(4,5,2))
+
+
+
+
+
+
